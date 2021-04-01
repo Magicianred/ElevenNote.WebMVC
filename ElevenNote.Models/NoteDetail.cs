3c50a702c67631585e5d8f68e7aa24f4da1ec723
annajoyw <arealjoyw@gmail.com>
2021-04-01 18:54:42 -0400

    details view

--- ElevenNote.Models/NoteDetail.cs
+++ ElevenNote.Models/NoteDetail.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace ElevenNote.Models
 {
-    class NoteDetail
+    public class NoteDetail
     {
         public int NoteId { get; set; }
         public string Title { get; set; }
